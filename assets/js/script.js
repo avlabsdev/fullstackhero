c4b93214db22042d14bdac17b84b45cb043a310f
fullStackHero <hello@avui.design>
2024-09-26 08:32:41 +0000

    html/js updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 let gridItem = document.getElementsByClassName("grid-item");
 let gridItemOverlay = document.getElementsByClassName('grid-item-overlay');
 
-for (let i = 0; i < gridItem.length; i++) {
-    gridItem[i].addEventListener('mouseover', function () {
-        gridItemOverlay[i].style.display = 'flex';
-    });
+// TODO: I will implement the below at a later date with modal popups for extra info on each service
+// for (let i = 0; i < gridItem.length; i++) {
+//     gridItem[i].addEventListener('mouseover', function () {
+//         gridItemOverlay[i].style.display = 'flex';
+//     });
 
-    gridItem[i].addEventListener('mouseout', function () {
-        gridItemOverlay[i].style.display = 'none';
-    });
-}
+//     gridItem[i].addEventListener('mouseout', function () {
+//         gridItemOverlay[i].style.display = 'none';
+//     });
+// }
 
 let submitBtn = document.getElementById('submitBtn');
 // Show results on submission here - redirect will take you/keep you at the Inquire section of the page to be able to view the results.
